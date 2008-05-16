cccd22aef79a245c04546508054eb2570c71e270
Mathieu Martin <webmat@gmail.com>
2008-05-16 00:35:37 -0400

    Finished the module rename

--- test/unit/intrusiveness_test.rb
+++ test/unit/intrusiveness_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 #These test names are made for rSpec, I know.
 
-class CaptureOutputNotIncludedByDefaultTest < Test::Unit::TestCase
+class CaptureFuNotIncludedByDefaultTest < Test::Unit::TestCase
 
   def test__method__not_polluting_namespace_when_not_explicitly_included
     assert_raise(NoMethodError) do
@@ -13,7 +13,7 @@ class CaptureOutputNotIncludedByDefaultTest < Test::Unit::TestCase
   end
 end
 
-class CaptureOutputNotIntrusiveWhenIncludedTest < Test::Unit::TestCase
+class CaptureFuNotIntrusiveWhenIncludedTest < Test::Unit::TestCase
   include CaptureFu
 
   def test__utility_classes__not_polluting_namespace_when_not_explicitly_included
